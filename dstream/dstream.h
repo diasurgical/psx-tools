344516269379541b823b69759e195fd13bd95196
John Törnblom <john.tornblom@gmail.com>
2020-04-21 12:18:10 +0200

    64bit compatibility

--- dstream/dstream.h
+++ dstream/dstream.h
@@ -1,52 +1,54 @@
 #ifndef __DSTREAM_H__
 #define __DSTREAM_H__
 
+#include <stdint.h>
+
 typedef struct VagBlock {
-	unsigned short block_num;
-	unsigned short unk02;
-	unsigned short block_size; // sizeof(VagBlock) + VAG_CHUNK
-	unsigned short unk06;
-	char unk08[120];
+	uint16_t block_num;
+	uint16_t unk02;
+	uint16_t block_size; // sizeof(VagBlock) + VAG_CHUNK
+	uint16_t unk06;
+	uint8_t unk08[120];
 } VagBlock;
 
 #define VAG_MAGIC    0x56414770 // 'VAGp'
 typedef struct VagHeader {
-	int magic;
-	int version;
-	int field8;
-	int size;
-	int rate;
-	int field14;
-	int field18;
-	int field1C;
+	int32_t magic;
+	int32_t version;
+	int32_t field8;
+	int32_t size;
+	int32_t rate;
+	int32_t field14;
+	int32_t field18;
+	int32_t field1C;
 	char name[16];
 } VagHeader;
 
 #ifndef OLD_STREAM_VERSION
 
 typedef struct STREAM {
-	unsigned long Offset;
-	unsigned long Size;
-	unsigned char Name[12];
+	uint32_t Offset;
+	uint32_t Size;
+	char Name[12];
 } STREAM;
 
 #else
 
 typedef struct STREAM {
-	unsigned long Offset;
-	int unk04;
-	unsigned long Size;
-	int unk0c[3];
-	unsigned char Name[12];
-	int unk24[9];
+	uint32_t Offset;
+	uint32_t unk04;
+	uint32_t Size;
+	uint32_t unk0c[3];
+	char Name[12];
+	uint32_t unk24[9];
 } STREAM;
 
 #endif
 
 #define LDIR_MAGIC   0x4c444952 // 'LDIR'
 typedef struct LDIRS {
-	int magic;
-	int ncount;
+	int32_t magic;
+	int32_t ncount;
 	STREAM stream;
 } LDIRS;
 
